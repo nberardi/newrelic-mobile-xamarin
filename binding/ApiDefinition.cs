50e2e43aeb21f9a65550cf091ff69c0257ff9442
Nick Berardi <nick@managedfusion.com>
2013-12-17 08:38:48 -0500

    updated bindings to 3.154 of NewRelic

--- binding/ApiDefinition.cs
+++ binding/ApiDefinition.cs
@@ -1,35 +1,137 @@
 using System;
-using MonoTouch.Foundation;
-using MonoTouch.CoreLocation;
 
-namespace NewRelic
-{
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+
+namespace NewRelic {
+
 	[BaseType (typeof (NSObject))]
-	public interface NewRelicAgent
-	{
+	public partial interface NRTimer {
+
+		[Export ("startTimeMillis")]
+		double StartTimeMillis { get; }
+
+		[Export ("endTimeMillis")]
+		double EndTimeMillis { get; }
+
+		[Export ("startTimeInMillis")]
+		double StartTimeInMillis { get; }
+
+		[Export ("endTimeInMillis")]
+		double EndTimeInMillis { get; }
+
+		[Export ("restartTimer")]
+		void RestartTimer ();
+
+		[Export ("stopTimer")]
+		void StopTimer ();
+
+		[Export ("hasRunAndFinished")]
+		bool HasRunAndFinished { get; }
+
+		[Export ("timeElapsedInSeconds")]
+		float TimeElapsedInSeconds { get; }
+
+		[Export ("timeElapsedInMilliSeconds")]
+		double TimeElapsedInMilliSeconds { get; }
+
+//		[DllImport ("/Users/nick/Projects/caddio/vendor/newrelic/binding/NewRelic_iOS_Agent_3.154/NewRelicAgent.framework/Versions/A/Headers/NRTimer.h")]
+//		double NR_NanosecondsFromTimeInterval (double timeInterval);
+	}
+
+	[BaseType (typeof (NSObject))]
+	public partial interface NRLogger {
+
+		[Static, Export ("log:inFile:atLine:inMethod:withMessage:")]
+		void Log (uint level, string file, uint line, string method, string message);
+
+		[Static, Export ("logLevels")]
+		uint LogLevels { set; }
+
+		[Static, Export ("logTargets")]
+		uint LogTargets { set; }
+
+		[Static, Export ("logFilePath")]
+		string LogFilePath { get; }
+
+		[Static, Export ("clearLog")]
+		void ClearLog ();
+	}
+
+	[BaseType (typeof (NSObject))]
+	public partial interface NewRelic {
+
 		[Static, Export ("startWithApplicationToken:")]
 		void StartWithApplicationToken (string appToken);
 
 		[Static, Export ("startWithApplicationToken:withoutSecurity:")]
-		void StartWithApplicationToken (string applicationToken, bool disableSsl);
+		void StartWithApplicationToken (string appToken, bool disableSSL);
 
-		[Static, Export ("setDeviceLocation:")]
-		void SetDeviceLocation (CLLocation options);
+		[Static, Export ("createAndStartTimer")]
+		NRTimer CreateAndStartTimer { get; }
+
+		[Static, Export ("startInteractionFromMethodName:object:")]
+		void StartInteractionFromMethodName (string selectorName, NSObject obj);
+
+		[Static, Export ("startInteractionFromMethodName:object:customizedName:")]
+		void StartInteractionFromMethodName (string selectorName, NSObject obj, string interactionName);
+
+		[Static, Export ("startInteractionFromMethodName:object:customizedName:cancelRunningTrace:")]
+		void StartInteractionFromMethodName (string selectorName, NSObject obj, string interactionName, bool cancel);
+
+		[Static, Export ("startTracingMethod:object:timer:category:")]
+		void StartTracingMethod (Selector selector, NSObject obj, NRTimer timer, NRTraceType category);
+
+		[Static, Export ("endTracingMethodWithTimer:")]
+		void EndTracingMethodWithTimer (NRTimer timer);
+
+		[Static, Export ("recordMetricWithName:category:")]
+		void RecordMetricWithName (string name, string category);
+
+		[Static, Export ("recordMetricWithName:category:value:")]
+		void RecordMetricWithName (string name, string category, NSNumber value);
+
+		[Static, Export ("recordMetricWithName:category:value:valueUnits:")]
+		void RecordMetricWithName (string name, string category, NSNumber value, string valueUnits);
+
+		[Static, Export ("recordMetricWithName:category:value:valueUnits:countUnits:")]
+		void RecordMetricWithName (string name, string category, NSNumber value, string valueUnits, string countUnits);
+
+		[Static, Export ("noticeNetworkRequestForURL:withTimer:responseHeaders:statusCode:bytesSent:bytesReceived:responseData:andParams:")]
+		void NoticeNetworkRequestForURL (NSUrl url, NRTimer timer, NSDictionary headers, int httpStatusCode, uint bytesSent, uint bytesReceived, NSData responseData, NSDictionary parameters);
+
+		[Static, Export ("noticeNetworkFailureForURL:withTimer:andFailureCode:")]
+		void NoticeNetworkFailureForURL (NSUrl url, NRTimer timer, int iOSFailureCode);
 	}
 
-	[BaseType(typeof(NSObject))]
-	public interface NRLogger
-	{
-		[Static, Export ("setLogLevels:")]
-		void SetLogLevels (int levels);
+	[BaseType (typeof (NSObject))]
+	public partial interface NRCustomMetrics {
 
-		[Static, Export ("setLogTargets:")]
-		void SetLogTargets (int targets);
+		[Static, Export ("recordMetricWithName:category:")]
+		void RecordMetricWithName (string name, string category);
 
-		[Static, Export("logFilePath")]
-		string LogFilePath { get; }
+		[Static, Export ("recordMetricWithName:category:value:")]
+		void RecordMetricWithName (string name, string category, NSNumber value);
 
-		[Static, Export("clearLog")]
-		void ClearLog ();
+		[Static, Export ("recordMetricWithName:category:value:valueUnits:")]
+		void RecordMetricWithName (string name, string category, NSNumber value, string valueUnits);
+
+		[Static, Export ("recordMetricWithName:category:value:valueUnits:countUnits:")]
+		void RecordMetricWithName (string name, string category, NSNumber value, string valueUnits, string countUnits);
+
+//		[DllImport ("/Users/nick/Projects/caddio/vendor/newrelic/binding/NewRelic_iOS_Agent_3.154/NewRelicAgent.framework/Versions/A/Headers/NRGCDOverride.h")]
+//		void NR__dispatch_async (NSObject queue, dispatch_block_t block);
+//
+//		[DllImport ("/Users/nick/Projects/caddio/vendor/newrelic/binding/NewRelic_iOS_Agent_3.154/NewRelicAgent.framework/Versions/A/Headers/NRGCDOverride.h")]
+//		void NR__dispatch_sync (NSObject queue, dispatch_block_t block);
+//
+//		[DllImport ("/Users/nick/Projects/caddio/vendor/newrelic/binding/NewRelic_iOS_Agent_3.154/NewRelicAgent.framework/Versions/A/Headers/NRGCDOverride.h")]
+//		void NR__dispatch_after (dispatch_time_t when, NSObject queue, dispatch_block_t block);
+//
+//		[DllImport ("/Users/nick/Projects/caddio/vendor/newrelic/binding/NewRelic_iOS_Agent_3.154/NewRelicAgent.framework/Versions/A/Headers/NRGCDOverride.h")]
+//		void NR__dispatch_apply (size_t iterations, NSObject queue, Delegate block);
+//
+//		[DllImport ("/Users/nick/Projects/caddio/vendor/newrelic/binding/NewRelic_iOS_Agent_3.154/NewRelicAgent.framework/Versions/A/Headers/NRGCDOverride.h")]
+//		void NR__dispatch_once ([unmapped: pointer: Pointer] once, dispatch_block_t block);
 	}
 }
--- binding/StructsAndEnums.cs
+++ binding/StructsAndEnums.cs
@@ -4,20 +4,76 @@ namespace NewRelic
 {
 	[Flags]
 	public enum NRLogLevels {
-		NRLogLevelNone    = 0,
-		NRLogLevelError   = 1 << 0,
-		NRLogLevelWarning = 1 << 1,
-		NRLogLevelInfo    = 1 << 2,
-		NRLogLevelVerbose = 1 << 3,
-		NRLogLevelALL     = 0xffff
+		None = 0,
+		Error = 1 << 0,
+		Warning = 1 << 1,
+		Info = 1 << 2,
+		Verbose = 1 << 3,
+		All = 0xffff
 	}
 
 	[Flags]
 	public enum NRLogTargets {
-		NRLogTargetNone      = 0,
-		NRLogTargetConsole   = 1 << 0,
-		NRLogTargetFile      = 1 << 1,
-		NRLogTargetALL       = 0xffff
+		None = 0,
+		Console = 1 << 0,
+		File = 1 << 1,
+		All = 0xfff
+	}
+
+	public enum NRTraceType {
+		None,
+		ViewLoading,
+		Layout,
+		Database,
+		Images,
+		Json,
+		Network
+	}
+
+	public enum NRNetworkFailureCode {
+		URLErrorUnknown = -1,
+		URLErrorCancelled = -999,
+		URLErrorBadURL = -1000,
+		URLErrorTimedOut = -1001,
+		URLErrorUnsupportedURL = -1002,
+		URLErrorCannotFindHost = -1003,
+		URLErrorCannotConnectToHost = -1004,
+		URLErrorDataLengthExceedsMaximum = -1103,
+		URLErrorNetworkConnectionLost = -1005,
+		URLErrorDNSLookupFailed = -1006,
+		URLErrorHTTPTooManyRedirects = -1007,
+		URLErrorResourceUnavailable = -1008,
+		URLErrorNotConnectedToInternet = -1009,
+		URLErrorRedirectToNonExistentLocation = -1010,
+		URLErrorBadServerResponse = -1011,
+		URLErrorUserCancelledAuthentication = -1012,
+		URLErrorUserAuthenticationRequired = -1013,
+		URLErrorZeroByteResource = -1014,
+		URLErrorCannotDecodeRawData = -1015,
+		URLErrorCannotDecodeContentData = -1016,
+		URLErrorCannotParseResponse = -1017,
+		URLErrorInternationalRoamingOff = -1018,
+		URLErrorCallIsActive = -1019,
+		URLErrorDataNotAllowed = -1020,
+		URLErrorRequestBodyStreamExhausted = -1021,
+		URLErrorFileDoesNotExist = -1100,
+		URLErrorFileIsDirectory = -1101,
+		URLErrorNoPermissionsToReadFile = -1102,
+		URLErrorSecureConnectionFailed = -1200,
+		URLErrorServerCertificateHasBadDate = -1201,
+		URLErrorServerCertificateUntrusted = -1202,
+		URLErrorServerCertificateHasUnknownRoot = -1203,
+		URLErrorServerCertificateNotYetValid = -1204,
+		URLErrorClientCertificateRejected = -1205,
+		URLErrorClientCertificateRequired = -1206,
+		URLErrorCannotLoadFromNetwork = -2000,
+		URLErrorCannotCreateFile = -3000,
+		URLErrorCannotOpenFile = -3001,
+		URLErrorCannotCloseFile = -3002,
+		URLErrorCannotWriteToFile = -3003,
+		URLErrorCannotRemoveFile = -3004,
+		URLErrorCannotMoveFile = -3005,
+		URLErrorDownloadDecodingFailedMidStream = -3006,
+		URLErrorDownloadDecodingFailedToComplete = -3007
 	}
 }
-
